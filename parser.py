76ab958101da365cd89dc008bcd7df50017cb2c2
Vsevolod Rymar <sevranty@sevranty.ru>
2019-10-13 16:24:52 +0300

    update

--- parser.py
+++ parser.py
@@ -6,7 +6,6 @@ import os
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 def parse_files(path):
     x_y, x = dict(), list()
     for file in glob.glob('%s/*.txt' % path):
@@ -16,9 +15,8 @@ def parse_files(path):
             raw_data = [c.split() for c in f_lines]
             x_y[f_name] = [{'x': int(float(a)), 'y': b.replace('.', ',')} for a, b in raw_data]
             x.extend(sorted(set([i['x'] for i in x_y[f_name]])))
-            logger.log(20, 'Parsed file %s' % f.name)
+            logger.log(20, ' → Parsed file "%s"' % f.name)
     return x, x_y
-
 
 x, x_y = parse_files('resources')
 result_dict = {i: [None] * len(x_y) for i in x}
@@ -30,9 +28,9 @@ for n, k in enumerate(x_y):
 
 redult_set = [(k,) + tuple(v) for k, v in result_dict.items()]
 
-with open('result.csv', 'w', encoding='utf-8') as fp:
+with open('[result].csv', 'w', encoding='utf-8') as fp:
     writer = csv.writer(fp, dialect='excel', lineterminator='\n')
     writer.writerow(['x'] + ['y_(%s)' % f for f in files])
     writer.writerows(sorted(redult_set, key=lambda s: s[0]))
-    logger.log(20, 'Finished writing to file %s' % fp.name)
+    logger.log(20, ' → Finished writing to file "%s"' % fp.name)
 
