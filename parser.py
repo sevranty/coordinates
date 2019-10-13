397384aad86c01e415adecb95e6bdfb753fa6b39
Vsevolod Rymar <sevranty@sevranty.ru>
2019-10-13 16:25:56 +0300

    update

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@ import os
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 def parse_files(path):
     x_y, x = dict(), list()
     for file in glob.glob('%s/*.txt' % path):
@@ -17,6 +18,7 @@ def parse_files(path):
             x.extend(sorted(set([i['x'] for i in x_y[f_name]])))
             logger.log(20, ' → Parsed file "%s"' % f.name)
     return x, x_y
+
 
 x, x_y = parse_files('resources')
 result_dict = {i: [None] * len(x_y) for i in x}
@@ -33,4 +35,3 @@ with open('[result].csv', 'w', encoding='utf-8') as fp:
     writer.writerow(['x'] + ['y_(%s)' % f for f in files])
     writer.writerows(sorted(redult_set, key=lambda s: s[0]))
     logger.log(20, ' → Finished writing to file "%s"' % fp.name)
-
